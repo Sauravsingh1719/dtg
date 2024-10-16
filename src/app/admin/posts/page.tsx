cd3e7fa7e46eb87efeda4cfa0f65780c25ef3b95
Saurav Singh <Sauravs1719@gmail.com>
2024-10-16 20:24:41 +0530

    boolean

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import AddPost from '../add-post/page';
 import { Dialog, DialogTrigger, DialogContent } from '@/components/ui/dialog';
-import AddPost from '@/components/AddPost';
 
 
 type PostType = {
